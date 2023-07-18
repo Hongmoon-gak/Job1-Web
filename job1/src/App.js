fbd3afa2e8c45427217fe9b9e78d949e564b5fe7
jh2ee <ljh20000321@gmail.com>
2023-07-18 14:40:14 +0900

    [FEAT] 로그인 버튼, navbar 레이아웃 구현

--- job1/src/App.js
+++ job1/src/App.js
@@ -5,6 +5,8 @@ import Home from "./View/Home";
 import Laws from "./View/Laws";
 import Board from "./View/Board";
 import News from "./View/News";
+import Login from "./View/Login";
+import MyPage from "./View/MyPage";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path='/law' element={<Laws/>} />
           <Route path='/board' element={<Board/>} />
           <Route path='/news' element={<News/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/mypage' element={<MyPage/>} />
         </Routes>
       </Router>
     </div>
