a0f8dfd7f07a1561428f9a4a9ad3ad906b6d03d7
J1 <ju1e3718@gmail.com>
2023-07-21 12:16:56 +0900

    [ADD] Preview Board 폰트 설정 추가 (#7)

--- job1/src/Main/PrevCont.js
+++ job1/src/Main/PrevCont.js
@@ -3,19 +3,19 @@ import PrevArt from "./PrevArt";
 function PrevCont() {
   return (
     <div className="cArtView">
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
       <hr />
-      <PrevArt title="제목" href="" likes="50" date="Date" />
+      <PrevArt title="Title" href="" likes="50" date="Date" />
     </div>
   );
 }
