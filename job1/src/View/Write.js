4ba65d5460c791f67ce96949e8c171d043aded4c
jh2ee <ljh20000321@gmail.com>
2023-07-25 20:42:58 +0900

    [FEAT] 게시글 작성 기능 구현 (#24)

--- job1/src/View/Write.js
+++ job1/src/View/Write.js
@@ -1,5 +1,66 @@
+
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
 function Write() {
-  return <div>Write</div>;
+  const [post, setPost] = useState({
+    title: '',
+    content: '',
+    author: '',
+    likes: '0'
+  });
+
+  const { title, content, author, likes } = post;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+  
+  const navigate = useNavigate();
+  const submit = (e) => {
+    e.preventDefault();
+    alert('등록되었습니다!');
+    navigate('/community');
+    console.log(post);
+  };
+
+  const cancel = () => {
+    navigate(`/community`);
+  };
+
+  return (
+    <form>
+      <div>
+        <p>글쓰기</p>
+        <hr/>
+        <input 
+          type="text" 
+          name="title" 
+          placeholder="제목" 
+          value={title}
+          onChange={onChange}
+        />
+        <textarea 
+          name="content" 
+          placeholder="내용을 입력하세요"
+          value={content}
+          onChange={onChange}
+        ></textarea>
+      </div>
+      <div>
+        <p>태그 선택</p>
+        <hr/>
+        {/* 태그 */}
+        <hr/>
+        <button onClick={submit}>등록</button>
+        <button onClick={cancel}>취소</button>
+      </div>
+    </form>
+  );
 }
 
 export default Write;
